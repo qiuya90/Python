60e267076467895338df06a6bf4d04bd82bac0b2
Ivan Chur <qiuya90@users.noreply.github.com>
2019-04-08 11:03:46 +0800

    Update main.py

--- Vision/car_count_better/main.py
+++ Vision/car_count_better/main.py
@@ -91,48 +91,52 @@ while(cap.isOpened()):
                 m=cv2.moments(cnt)
                 cx=int(m['m10']/m['m00'])
                 cy=int(m['m01']/m['m00'])
-                x,y,w,h=cv2.boundingRect(cnt)
+                x,y,w,h=cv2.boundingRect(cnt)                                                       #得到最小正矩形的各项参数，x，y为矩形左上角的坐标
 
-                new=True
+                new = True
                 if cy in range(up_limit,down_limit):
                     for i in cars:
-                        if abs(x - i.getX()) <= w and abs(y - i.getY()) <= h:
-                            new = False
-                            i.updateCoords(cx, cy)
+                        if abs(x - i.getX()) <= w and abs(y - i.getY()) <= h:                       #当前块的左上角点减去上一帧质心 是否小于当前最小矩形宽高
+                            new = False                                                             #如果是，则不是新的块，记为False
+                            i.updateCoords(cx, cy)                                                  #更新该车辆块的质心到当前
 
-                            if i.going_UP(line_down,line_up)==True:
-                                cnt_up+=1
-                                print("ID:",i.getId(),'crossed going up at', time.strftime("%c"))
-                            elif i.going_DOWN(line_down,line_up)==True:
-                                cnt_down+=1
-                                print("ID:", i.getId(), 'crossed going up at', time.strftime("%c"))
-                            break
-                        if i.getState()=='1':
-                            if i.getDir()=='down'and i.getY()>down_limit:
-                                i.setDone()
-                            elif i.getDir()=='up'and i.getY()<up_limit:
-                                i.setDone()
-                        if i.timedOut():
-                            index=cars.index(i)
-                            cars.pop(index)
-                            del i
+                            if i.going_UP(line_down,line_up)==True:                                 #判断是否向上行驶，并过计数线上，若反馈为真，则统计一次向上的计数
+                                cnt_up+=1                                                           #统计向上车辆数+1
+                                print("ID:",i.getId(),'crossed going up at', time.strftime("%c"))   #控制台打印出该车辆的行驶信息
 
-                    if new==True: #If nothing is detected,create new
-                        p=vehicles.Car(pid,cx,cy,max_p_age)
-                        cars.append(p)
-                        pid+=1
+                            elif i.going_DOWN(line_down,line_up)==True:                             #判断是否向下行驶，并过计数线下，若反馈为真，则统计一次向下的计数
+                                cnt_down+=1                                                         #统计向下车辆数+1
+                                print("ID:", i.getId(), 'crossed going up at', time.strftime("%c")) #控制台打印出该车辆的行驶信息
+                            break                                                                   #因为不是新的块，执行完成后break退出当前i，进行下一个i的操作；
+                                                                                                    # 知识点：break是跳出目前这一层的for循环，且break语句对if-else的条件语句不起作用
 
-                cv2.circle(frame,(cx,cy),5,(0,0,255),-1)
-                img=cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+                        if i.getState()=='1':                                                       #vehicles.py反馈i.state的状态，默认为零，若被统计过则为1；意味着不满足上述的一些条件，
+                                                                                                    #或者不是新块且被统计过，或者是新块但轨迹还没被统计过两次
+                            if i.getDir()=='down'and i.getY()>down_limit:                           #如果方向向下，且过了最下的线
+                                i.setDone()                                                         #则该车辆信息在vehicles中的self.done置为真
+                            elif i.getDir()=='up'and i.getY()<up_limit:                             #如果方向向上，且过了最上的线
+                                i.setDone()                                                         #则该车辆信息在vehicles中的self.done置为真
 
-        for i in cars:
+                        if i.timedOut():                                                            #判断车辆信息在vehicles中的self.done的值
+                            index=cars.index(i)                                                     #获取i在cars中的位置值
+                            cars.pop(index)                                                         #将该值踢出cars
+                            del i                                                                   #删除该变量
+
+                    if new == True: #If nothing is detected,create new                              #判断如果是新的块
+                        p=vehicles.Car(pid,cx,cy,max_p_age)                                         #得到该新的块的信息放入p；pid其实是出现过的块的数量总计
+                        cars.append(p)                                                              #数据添加到cars当中
+                        pid+=1                                                                      #块的计数+1
+
+                cv2.circle(frame,(cx,cy),5,(0,0,255),-1)                                            #对大于规定面积的块，标记质心
+                img=cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)                                #对大于规定面积的块，画出最小的正矩形
+
+        for i in cars:                                                                              #对所有出现的车辆信息显示id文字信息
             cv2.putText(frame, str(i.getId()), (i.getX(), i.getY()), font, 0.3, i.getRGB(), 1, cv2.LINE_AA)
-
-
 
 
         str_up='UP: '+str(cnt_up)
         str_down='DOWN: '+str(cnt_down)
+
         frame=cv2.polylines(frame,[pts_L1],False,line_down_color,thickness=2)
         frame=cv2.polylines(frame,[pts_L2],False,line_up_color,thickness=2)
         frame=cv2.polylines(frame,[pts_L3],False,(255,255,255),thickness=1)
@@ -143,20 +147,10 @@ while(cap.isOpened()):
         cv2.putText(frame, str_down, (10, 90), font, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
         cv2.imshow('Frame',frame)
 
-        if cv2.waitKey(1)&0xff==ord('q'):
+        if cv2.waitKey(2)&0xff==ord('q'):
             break
-
     else:
         break
 
 cap.release()
 cv2.destroyAllWindows()
-
-
-
-
-
-
-
-
-
